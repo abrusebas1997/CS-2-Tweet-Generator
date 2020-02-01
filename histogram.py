b466e26ca0b5edee031cfd5fc6473362159a1278
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-01-31 20:25:09 -0800

    adding histogram and frequency

--- histogram.py
+++ histogram.py
@@ -2,19 +2,27 @@ import sys
 
 
 
-def open_file(file_content):
+def open_file():
     f_histogram = open("book.txt", "r")
     file_content = []
 
     for line in f_histogram:
         # split creates another list, if you use append, you will create a list of lists; but with extend you create just 1
         file_content.extend(line.split())
+    return file_content
 
 def histogram():
     """ take a filename or content_file as
      a string and return histogram data
      that stores words with the number of times the word appears"""
-    pass
+     histogram = {}
+     for word in file_content:
+
+         if word in histogram:
+            histogram[word] += 1
+         else:
+            histogram[word] = 1
+    return histogram
 
 def unique_words(histogram):
     """returns the total count of unique words"""
@@ -22,4 +30,6 @@ def unique_words(histogram):
 
 def frequency(word, histogram):
     """returns the number of times that word appears in the  text"""
-    pass
+    for key, value in histogram.items():
+        if key == word:
+            return value
