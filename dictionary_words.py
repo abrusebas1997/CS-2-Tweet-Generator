17dd1f4bb3edb73893b55b0731bacd474d9cd26b
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-01-31 18:11:40 -0800

    adding dictionary_words.py

---
+++ dictionary_words.py
@@ -0,0 +1,28 @@
+"""read in the words file/ select random words from the file and store them in a data type/ number of words requested together into a sentence"""
+import random
+import sys
+
+work_file = []
+# looping through all the words and stipping them to get a space
+f = open("/usr/share/dict/words", "r")
+for line in f:
+    work_file.append(line.strip())
+
+def random_sentence(words, length):
+    random_sentence = " "
+
+    for _ in range(length):
+        random_list = random.randint(0, len(words) - 1)
+        random_sentence += " " + words[random_list]
+        words.remove(words[random_list])
+    print(random_sentence)
+
+if __name__ == "__main__":
+    parameters = sys.argv[1:]
+    number_words = int(parameters[0])
+    if number_words < 2:
+        print("You need more words to make a sentence")
+    elif number_words == None:
+        print("Enter something")
+    else:
+        random_sentence(work_file, number_words)
---
+++ quote_user.py
@@ -0,0 +1,6 @@
+# import python_quote
+#
+#
+# if __name__ == '__main__':
+#     quote = random_python_quote()
+#     print(quote)
