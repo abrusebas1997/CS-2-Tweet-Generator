ef969b5908afae55f8be95e5f7bc117697505e4b
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-02-19 23:29:07 -0800

    [added] sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,31 @@
-# # take the words from histogram.py and make sentences with them
-#
-#
-# import random
-# from histogram import open_file, histogram, frequency, total_words
-#
-# histogram = {"one" "fish" "two" "fish" "red" "fish" "blue" "fish"}
-#
-# def one(dic, total):
-#   rand_num = random.randint(1, total)
-#   total_value = 0
-#   for key,value in dic.items():
-#     if rand_num <= total_value:
-#       return key
-#     else:
-#       total_value += value
+import random
+from histogram import open_file, histogram, frequency
+
+
+def words_in_total(histogram):
+    total = 0
+    for value in histogram.values():
+        total += value
+    return total
+
+    # total number of words in the file(values)
+
+
+histogram = (histogram(open_file('book.txt')))
+
+total_words = words_in_total(histogram)
+# print(total_words)
+
+def stochastic(dict, total_words):  #ask cao why dic
+    random_number = random.randint(1, total_words)
+    total_value = 0
+    for word, value in dict.items():
+        if random_number - value - total_value <= 0:
+            return word
+        else:
+            total_value += value
+
+
+# when you run this for loop, it prints one random word of test.txt 1000 times
+# for i in range(1000):
+#     print(stochastic(histogram, total_words))
